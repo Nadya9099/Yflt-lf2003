678fe28ba7ac2a9df14d83c10e953dc141986458
Nadya9099 <81128404+Nadya9099@users.noreply.github.com>
2021-03-22 11:34:37 +0700

    Add files via upload

---
+++ style.css
@@ -0,0 +1,148 @@
+body {
+    padding: 0 5;
+    font-size: 14px;
+    line-height: 22px;
+    font-family: "Georgia", serif;
+    color: #222222;
+    highte: 33px;
+    }
+    
+    h1 {
+    text-align: center;
+    color: white;
+    background: rgb(76, 117, 253);
+    }
+    
+    div {
+    background-color: #8ebbff;
+    color:white;
+    font-size: 20px;
+    border: 1px solid #ffffff;
+    margin: 10px;
+    display: inline-block;
+    text-align: center
+    }
+    
+    header {
+    background: #8ebbff;
+    height: 70px;
+    margin-bottom: 0px;
+    }
+    
+    aside {
+    margin: -10px 0 5px;
+    margin-bottom: 10px;
+    height: 500px;
+    width: 40%;
+    float: left;
+    }
+    section {
+    margin: -10px 0 5px;
+    margin-bottom: 10px;
+    float: left;
+    height: 500px;
+    width: 60%;
+    text-align: center;
+    background-color: rgb(76, 117, 253);
+    }
+    
+    
+    article {
+    float: left;
+    width: 60%;
+    color: white;
+    background: rgbrgb(0, 0, 197);
+    margin: 20px 0 5px;
+    }
+    
+    span {
+    width: 40%;
+    float: left;
+    }
+    
+    div {
+    font-size: 20px;
+    background-color: #2d4af0;
+    color:white;
+    border: 3 px solid white;
+    }
+    
+    img {
+    text-align: right;
+    }
+    
+    
+    aside {
+    background-color: #8ebbff;
+    color:white;
+    }
+    
+    p {
+    margin: -5px 0 5px;
+    background-color: rgb(0, 0, 197);
+    color:white;
+    
+    
+    }
+    
+    footer {
+    margin: -20px 0 5px;
+    height: 50px;
+    text-align: center;
+    background-color: #002df3;
+    
+    }
+    img {
+    border: 5px solid blue;
+    }
+    
+    #wer>li{
+        padding:10px ;
+        background-color: rgb(0, 0, 131);
+        margin:10px 10px 10px 10px;
+        color:#fff
+    
+    
+    }#wer>li>a{
+        color:#fff;
+        text-decoration: none;
+    
+    }
+    *{
+        margin:0;
+        padding:0;
+    }
+    h2 {
+        text-align: center;
+        margin-left: 1px;
+        font-size: 25px;
+    }
+    
+    nav {
+    padding-bottom: 20px;
+    }
+    fieldset{
+        color:rgb(0, 0, 131);
+        margin:15px;
+        border:solid;
+        padding: 12px 20px ;
+        width: px;
+        font-size: 18px;
+    }
+    #rta >div{
+padding:20px;
+background:rgb(0, 0, 187);
+color:white;
+margin:15px;
+/* text-align: center; */
+margin-left:10px;
+padding-left: 7px;
+display: inline-block;
+text-align: center;
+style="list-style-type:none"
+}
+
+html{
+height:"600px"
+ width:"600px"
+}
